9434cc93443e91eb355a3461235d8a8ce2b597ff
rjayne <rachaeljayne@gmail.com>
2020-07-22 17:15:13 +1000

    Game Selection screen layout added

--- app/src/main/java/au/com/jayne/dogquiz/feature/main/dagger/MainActivityModule.kt
+++ app/src/main/java/au/com/jayne/dogquiz/feature/main/dagger/MainActivityModule.kt
@@ -1,7 +1,9 @@
 package au.com.jayne.dogquiz.feature.main.dagger
 
 import au.com.jayne.dogquiz.common.preference.YesNoPreferenceDialogFragment
+import au.com.jayne.dogquiz.feature.game.GameFragment
 import au.com.jayne.dogquiz.feature.main.MainActivity
+import au.com.jayne.dogquiz.feature.selection.GameSelectionFragment
 import au.com.jayne.dogquiz.feature.settings.SettingsFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -11,6 +13,12 @@ abstract class MainActivityModule {
 
     @ContributesAndroidInjector
     internal abstract fun provideMainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    abstract fun provideGameSelectionFragment(): GameSelectionFragment
+
+    @ContributesAndroidInjector
+    abstract fun provideGamenFragment(): GameFragment
 
     @ContributesAndroidInjector
     abstract fun provideYesNoPreferenceDialogFragment(): YesNoPreferenceDialogFragment
---
+++ app/src/main/java/au/com/jayne/dogquiz/feature/selection/GameSelectionFragment.kt
@@ -0,0 +1,4 @@
+package au.com.jayne.dogquiz.feature.selection
+
+class GameSelectionFragment {
+}
