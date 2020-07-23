4e4295a2e771945e5e637c11c60a84d27a376441
rjayne <rachaeljayne@gmail.com>
2020-07-23 23:38:44 +1000

    Fix button layout and alert dialog text button color.

--- app/src/main/java/au/com/jayne/dogquiz/common/extensions/FragmentExtensions.kt
+++ app/src/main/java/au/com/jayne/dogquiz/common/extensions/FragmentExtensions.kt
@@ -4,11 +4,12 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import timber.log.Timber
 
 fun Fragment.getPositiveOkButtonDialog(title: Int, message: Int?, onDismissListener: DialogInterface.OnDismissListener? = null): AlertDialog? {
     context?.let {
-        val builder = AlertDialog.Builder(it)
+        val builder = MaterialAlertDialogBuilder(it)
         builder.setTitle(getString(title))
         message?.let{
             builder.setMessage(getString(it))
